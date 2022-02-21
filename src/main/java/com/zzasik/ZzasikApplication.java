79556a62db50eeb1ea4ba248dd5c170596f0e178
jiyeonyh <wldus8827@gmail.com>
2022-02-21 14:23:19 +0900

    Initial Commit

---
+++ src/main/java/com/zzasik/ZzasikApplication.java
@@ -0,0 +1,13 @@
+package com.zzasik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzasikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzasikApplication.class, args);
+	}
+
+}
