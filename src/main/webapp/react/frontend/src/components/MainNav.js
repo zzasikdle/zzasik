eef8b63d28d82c92a3382175220df6b0db8e0233
woosolkim <woosol2da@gmail.com>
2022-03-02 11:28:44 +0900

    0302 front

---
+++ src/main/webapp/react/frontend/src/components/MainNav.js
@@ -0,0 +1,75 @@
+import React ,{useState, useEffect} from 'react'
+import {Nav,Navbar,Container,Offcanvas,NavDropdown,Form,FormControl,Button} from 'react-bootstrap';
+import {Link, Route, Switch} from 'react-router-dom';
+import Rotate from 'react-reveal/Rotate';
+
+
+function MainNav(props) {
+  
+  const [ScrollActive, setScrollActive] = useState(true);
+
+
+  const [ScrollY, setScrollY] = useState(0); // window 의 pageYOffset값을 저장 
+  function handleScroll() { 
+      if(ScrollY > 299) {
+          setScrollY(window.pageYOffset);
+          setScrollActive(true);
+      } else {
+          setScrollY(window.pageYOffset);
+          setScrollActive(false);
+      }
+  }
+  useEffect(() => {
+      function scrollListener() {  window.addEventListener("scroll", handleScroll); } //  window 에서 스크롤을 감시 시작
+      scrollListener(); // window 에서 스크롤을 감시
+      return () => { window.removeEventListener("scroll", handleScroll); }; //  window 에서 스크롤을 감시를 종료
+  });
+    return(
+        <div className={ScrollActive ? "main-nav-fixed" : "main-nav"}>
+
+
+      <Navbar bg="light" expand={false} fixed="top">
+  <Container fluid>
+    <Rotate>
+    <Navbar.Brand href="#">ZZASIK</Navbar.Brand>
+    </Rotate>
+    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+    <Navbar.Offcanvas
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+      placement="end"
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title id="offcanvasNavbarLabel">ZZASIK</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav.Link href="#action1">Home</Nav.Link>
+          <Nav.Link href="#action2">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">
+              Something else here
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Offcanvas.Body>
+    </Navbar.Offcanvas>
+  </Container>
+</Navbar>
+        </div>
+    )
+}
+
+export default MainNav;
