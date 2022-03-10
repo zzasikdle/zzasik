1ce2fe235da7f14ac03f814d9500cfa57b5f9a76
jiyeonyh <wldus8827@gmail.com>
2022-03-10 14:56:40 +0900

    test

--- src/main/webapp/react/frontend/src/components/notice/NoticePage.js
+++ src/main/webapp/react/frontend/src/components/notice/NoticePage.js
@@ -6,7 +6,7 @@ function NoticeView(){
     return (
         
         <div className="notice-container">
-            <div className='title-box'>짜식 공지사항</div>
+            <div className='title-box'>짜식 공지사항!</div>
             <div className='content'>
             {NoticeList()}
             <button className="writeBtn"><Link to='/notice/new' style={{ textDecoration: 'none', color:"white" }}>글쓰기</Link></button>
