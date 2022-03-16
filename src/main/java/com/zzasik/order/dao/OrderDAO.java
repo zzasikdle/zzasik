3101ba1e3d1aa4f8a82e1b1d10e3341b0e384fbd
hyekyoung123 <hyekyoung1996@gmail.com>
2022-03-16 22:35:51 +0900

    order 수정

--- src/main/java/com/zzasik/order/dao/OrderDAO.java
+++ src/main/java/com/zzasik/order/dao/OrderDAO.java
@@ -17,21 +17,3 @@ public interface OrderDAO {
 	public List<OrderVO> selectOrderList(String user_id);
 	public int deleteOrder(int order_code);
 }
-package com.zzasik.order.dao;
-
-import java.util.List;
-import java.util.Map;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
-import com.zzasik.order.vo.OrderVO;
-
-@Mapper
-@Repository("orderDAO")
-public interface OrderDAO {
-	public int insertOrder(Map<String, Object> orderMap);
-	public OrderVO selectOrderOne(int order_id);
-	public int selectOrderCode();
-	public List<OrderVO> selectOrderList(String user_id);
-}
--- src/main/java/com/zzasik/order/service/OrderService.java
+++ src/main/java/com/zzasik/order/service/OrderService.java
@@ -12,16 +12,3 @@ public interface OrderService {
 	public List<OrderVO> getOrderList(String user_id);
 	public boolean deleteProduct(int order_code);
 }
-package com.zzasik.order.service;
-
-import java.util.List;
-import java.util.Map;
-
-import com.zzasik.order.vo.OrderVO;
-
-public interface OrderService {
-	public boolean AddOrder(Map<String, Object> orderMap);
-	public OrderVO CheckProduct(int order_id);
-	public String GetMaxOrderCode();
-	public List<OrderVO> getOrderList(String user_id);
-}
