3f8493d6766b6301de4339ed0aabeb359235282d
woosolkim <woosol2da@gmail.com>
2022-03-18 09:59:45 +0900

    /*eslint-disable*/

--- src/main/webapp/react/frontend/src/mypage/user/page/MyLesson.js
+++ src/main/webapp/react/frontend/src/mypage/user/page/MyLesson.js
@@ -1,3 +1,6 @@
+/*eslint-disable*/
+
+
 import React, { useState} from 'react';
 import axios from 'axios';
 
--- src/main/webapp/react/frontend/src/product/InputFile.js
+++ src/main/webapp/react/frontend/src/product/InputFile.js
@@ -1,3 +1,6 @@
+/*eslint-disable*/
+
+
 import { useState } from "react";
 import ImageUpload from "../service/ImageUploader";
 
